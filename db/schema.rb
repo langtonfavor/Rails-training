3879ff5f7a765bc3acb48b2a2e939290c4c810e0
Langton <langtonfavor@gmail.com.com>
2021-11-08 09:53:35 +0200

    fix database configs

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_11_05_075935) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
